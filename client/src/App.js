e570c7fdb6dbbea6eb7ca9f2032f7b72e5faf6ce
HannahMarieWieser <nikauli@gmail.com>
2019-07-22 18:53:04 -0400

    finished step one

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,12 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+
+      <div>
+        <Route path="/" exact component={MovieList} />
+        <Route path="/movies/id" component={Movie} />
+      </div>
+
     </div>
   );
 };
